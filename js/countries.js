188869bc3c6e3c338181218b957271abad66bcd8
rashedrk <rashedrk001@gmail.com>
2023-02-25 23:59:43 +0600

    dynamic data load using api

--- js/countries.js
+++ js/countries.js
@@ -13,10 +13,26 @@ const displayCountries = countries => {
         countryDiv.innerHTML = `
             <h4>Name: ${country.name.common}</h4>
             <p>Capital: ${country.capital ? country.capital[0] : 'No capital'}</p>
+            <button onclick="loadCountryDetails('${country.cca2}')"> Details</button>
         `;
         countriesContainer.appendChild(countryDiv);
     });
     
 };
+
+const loadCountryDetails = code => {
+    const url = `https://restcountries.com/v3.1/alpha/${code}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showCountryDetails(data[0]))
+}
+
+const showCountryDetails = country => {
+    const detailsContainer = document.getElementById('country-details');
+    detailsContainer.innerHTML = `
+        <img src="${country.flags.png}" alt="">
+        <h3>Name: ${country.name.common}</h3>
+    `;
+}
 
 loadCountries();

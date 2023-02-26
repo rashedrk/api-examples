e0f343c35b06d4f0edee5d4c95925ce5c3135f1b
rashedrk <rashedrk001@gmail.com>
2023-02-26 23:02:18 +0600

    meal db api use

---
+++ js/meal.js
@@ -0,0 +1,26 @@
+const loadMeals = () => {
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Fish')
+    .then(res => res.json())
+    .then(data => displayMeals(data.meals))
+};
+
+const displayMeals = meals => {
+    const mealContainer = document.getElementById('meal-container');
+    meals.forEach(meal => {
+        console.log(meal);
+        const mealDiv = document.createElement('div');
+        mealDiv.classList.add('col');
+        mealDiv.innerHTML = `
+            <div class="card h-100">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        `;
+        mealContainer.appendChild(mealDiv);
+    });
+};
+
+loadMeals();
